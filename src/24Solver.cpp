317dd55c735ef184c111817524d0a3290f580489
MarcelRyan <marcel.ryan2004@gmail.com>
2023-01-22 01:03:29 +0700

    fix lagi dikit

--- src/24Solver.cpp
+++ src/24Solver.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int cekInputValidKartu(string input) {
     // Function untuk mengecek apakah input kartu valid atau tidak
-    if (input == "A" || input == "Q" || input == "J" || input == "K" || input == "2" || input == "3" || input == "4" || input == "5" || input == "6" || input == "7" || input == "8" || input == "9" || input == "10") {
+    if (input == "A" || input == "Q" || input == "J" || input == "K" || input == "1" || input == "2" || input == "3" || input == "4" || input == "5" || input == "6" || input == "7" || input == "8" || input == "9" || input == "10") {
         return 1;
     }
     else  {
